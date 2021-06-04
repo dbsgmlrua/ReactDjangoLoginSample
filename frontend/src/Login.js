8639597c7bfded8491146b47552c469e24341fa8
dbsgmlrua <dbsgmlrua@naver.com>
2021-06-04 16:51:40 +0900

    axios정리

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,18 +1,13 @@
-import useAxios from "./useAxios";
-import userFetch from "./userFetch";
-import { useEffect } from 'react';
-import axiosInstance from "./axiosApi";
+import useGetAxios from "./useAxios";
 
 const Login = () => {
 
     // const {data, isPending, error} = userFetch('http://localhost:8000/api/newhello/');
-    const {data, isPending, error} = useAxios('/newhello/');
+    const {data, isPending, error} = useGetAxios('/newhello/');
 
     return ( <div className="Login">
+        <h2>{isPending && <div>Loading..</div>}</h2>
         <h2>{data && data.hello}</h2> 
-        {/* <h2>{isPending && <div>Loading..</div>}</h2>
-        <h2>{error && <div>{error}</div>}</h2>
-        <h2>{blogs && blogs.hello}</h2> */}
     </div> );
 }
  
