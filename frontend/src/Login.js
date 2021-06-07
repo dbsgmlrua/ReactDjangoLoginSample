3efc2edaee76ea93a427d66c2d439a037617c29a
윤희겸 <dbsgmlrua@naver.com>
2021-06-07 23:07:29 +0900

    외않되

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,13 +1,51 @@
 import useGetAxios from "./useAxios";
+import { useState } from "react";
+
+const loginForm ={
+    id: "",
+    pw: ""
+}
 
 const Login = () => {
+    const [state, setState] = useState(loginForm);
+    const [activateBtn, setActivateBtn] = useState(true);
+    
+    const handleEachInput = (e) => {
+        const { id, value } = e.target;
+        setState({
+            [id]: value,
+        });
+        console.log(state);
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(state);
+    };
 
-    // const {data, isPending, error} = userFetch('http://localhost:8000/api/newhello/');
-    const {data, isPending, error} = useGetAxios('/newhello/');
+    return ( 
+    <div className="Login">
+        <form>
+            <input
+                type="text"
+                placeholder="전화번호, 사용자 이름 또는 이메일"
+                className="login_input"
+                id="id"
+                onChange={handleEachInput}
+            />
+            <input
+                type="password"
+                placeholder="비밀번호"
+                className="password_input"
+                id="pw"
+                onChange={handleEachInput}
+            />
 
-    return ( <div className="Login">
-        <h2>{isPending && <div>Loading..</div>}</h2>
-        <h2>{data && data.hello}</h2> 
+            <div>
+                <button onClick={handleSubmit} disabled={!activateBtn}>
+                로그인
+                </button>
+            </div>
+        </form>
     </div> );
 }
  
